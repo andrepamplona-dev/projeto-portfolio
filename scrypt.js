2bc5f123a69937bf7de9768ca1b5a08b4e3444dd
André Pamplona <pamplonaandrezinho>
2025-07-14 15:58:44 -0300

    Upload inicial: HTML, CSS e JavaScript básico

---
+++ scrypt.js
@@ -0,0 +1,9 @@
+<script>
+  const toggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+</script>
+
